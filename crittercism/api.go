abcb6a408e892f8ef992ef23fdaa7925149ccaf4
Gersham Meharg <gersham@meharg.com>
2014-10-17 15:15:01 -0700

    code cleanup

--- crittercism/api.go
+++ crittercism/api.go
@@ -11,28 +11,91 @@ import (
 	"time"
 )
 
-var token string
-var tokenExpires time.Time
+const crittercismAPIURL = "https://developers.crittercism.com:443/v1.0"
 
-func Request(method string, path string, params string) (*jsonq.JsonQuery, error) {
+var accessToken string = ""
+var refreshToken string = ""
+var accessTokenExpires time.Time
 
-	url := fmt.Sprintf("https://developers.crittercism.com:443/v1.0/%s", path)
+// Request will make a request of the Crittercism API
+// This will return a github.com/jmoiron/jsonq JSON query object
+func Request(method string, path string, params string) (jq *jsonq.JsonQuery, err error) {
 
+	// Check for accessToken, getting a new one if needed
+	if accessToken == "" || time.Now().After(accessTokenExpires) {
+		if token, expires, err := getOAuthToken(); err == nil {
+			accessToken = token
+			accessTokenExpires = time.Now().Add(time.Duration(expires) * time.Second)
+		} else {
+			return nil, err
+		}
+	}
+
+	// Construct REST Request
+	url := fmt.Sprintf("%s/%s", crittercismAPIURL, path)
 	p := []byte(params)
-
 	client := &http.Client{}
 	req, _ := http.NewRequest(method, url, bytes.NewBuffer(p))
-	req.Header.Set("Authorization", "Bearer cg299DBJDbZfLJq621uFewRlzvwDjKZM")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
 	req.Header.Set("Content-Type", "application/json")
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	// Make Request
+	if resp, err := client.Do(req); err == nil {
+		defer resp.Body.Close()
 
-	data := map[string]interface{}{}
-	dec := json.NewDecoder(strings.NewReader(string(body)))
-	dec.Decode(&data)
-	jq := jsonq.NewQuery(data)
+		// Parse Body
+		if body, err := ioutil.ReadAll(resp.Body); err == nil {
+			data := map[string]interface{}{}
+			dec := json.NewDecoder(strings.NewReader(string(body)))
+			dec.Decode(&data)
+			jq = jsonq.NewQuery(data)
+			return jq, nil
 
-	return jq, err
+		} else {
+			return nil, err // Parse Error
+		}
+
+	} else {
+		return nil, err // Request Error
+	}
+}
+
+// getOAuthToken fetches a new OAuth Token from the Crittercism API given a username and password
+func getOAuthToken() (token string, expires int, err error) {
+
+	// TODO - remove this once I get a password object
+	return "cg299DBJDbZfLJq621uFewRlzvwDjKZM", 0, nil
+
+	var params = ""
+
+	// Construct REST Request
+	url := fmt.Sprintf("%s/token", crittercismAPIURL)
+	p := []byte(params)
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(p))
+	req.Header.Set("Content-Type", "application/json")
+
+	// Make Request
+	if resp, err := client.Do(req); err == nil {
+		defer resp.Body.Close()
+
+		// Parse JSON
+		if body, err := ioutil.ReadAll(resp.Body); err == nil {
+			data := map[string]interface{}{}
+			dec := json.NewDecoder(strings.NewReader(string(body)))
+			dec.Decode(&data)
+			jq := jsonq.NewQuery(data)
+
+			// Parse out the token
+			token, _ := jq.String("access_token")
+			expires, _ := jq.Int("expires")
+			return token, expires, nil
+
+		} else {
+			return "", 0, err // Parse Error
+		}
+
+	} else {
+		return "", 0, err // Request Error
+	}
 }
--- flows/daily_app_loads.go
+++ flows/daily_app_loads.go
@@ -1,6 +1,7 @@
 package flows
 
 import (
+	"fmt"
 	"github.com/telemetryapp/agent_crittercism/crittercism"
 	"github.com/telemetryapp/gotelemetry"
 	"log"
@@ -8,33 +9,39 @@ import (
 )
 
 // Our interval to update this Flow on
-const interval = 60 * time.Second
+const interval = 60
 
 // DailyAppLoads will query the Crittercism API and get the daily app loads
 // It will then emit a Flow object to the flowChan for sending up to Telemetry
 func DailyAppLoads(flowChan chan gotelemetry.Flow) {
+
 	// Loop indefinitely, querying every interval seconds
 	for {
-		// Get the data from the API
-		if jq, err := crittercism.Request(
-			"POST",
-			"errorMonitoring/graph",
-			`{"params":{"graph": "appLoads","duration": 1440,"appId": "519d53101386202089000007"}}`); err == nil {
+		loopStartTime := time.Now()
 
+		// Build the Request of Crittercism
+		params := fmt.Sprintf(`{"params":{"graph": "appLoads", "duration": 1440, "appId": "%s"}}`, "519d53101386202089000007")
+		path := "errorMonitoring/graph"
+
+		// Get the data from Crittercism
+		if jq, err := crittercism.Request("POST", path, params); err == nil {
 			if loads, err := jq.Float("data", "series", "0", "points", "0"); err == nil {
-				// Parse the result and construct the flow
+
+				// Parse the result and construct the flow then emit to the channel
 				data := gotelemetry.Value{Label: "Daily App Loads", Value: loads}
 				flow := gotelemetry.NewFlow("daily_app_loads", &data)
-
-				// Emit to the channel
 				flowChan <- *flow
 
 			} else {
-				log.Print("Error ", err)
+				log.Print("Error ", err) // Parsing Error
 			}
 		} else {
-			log.Print("Error ", err)
+			log.Print("Error ", err) // Request Error
 		}
-		time.Sleep(interval)
+
+		// Sleep for the next loop
+		if sleepTime := interval - time.Since(loopStartTime).Seconds(); sleepTime > 0 {
+			time.Sleep(time.Duration(sleepTime) * time.Second)
+		}
 	}
 }
