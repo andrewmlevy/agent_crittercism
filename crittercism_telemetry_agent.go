612729180df64e683266a7b74f5d34b3dd9a4be8
Marco Tabini <marcot@Marcobook-Pro.local>
2015-05-22 08:21:47 -0400

    - Update to latest agent version

--- crittercism_telemetry_agent.go
+++ crittercism_telemetry_agent.go
@@ -1,41 +1,110 @@
 package main
 
 import (
+	"github.com/telemetryapp/gotelemetry"
+	"github.com/telemetryapp/gotelemetry_agent/agent"
+	"github.com/telemetryapp/gotelemetry_agent/agent/aggregations"
 	"github.com/telemetryapp/gotelemetry_agent/agent/config"
+	"github.com/telemetryapp/gotelemetry_agent/agent/functions"
+	"github.com/telemetryapp/gotelemetry_agent/agent/graphite"
 	"github.com/telemetryapp/gotelemetry_agent/agent/job"
+	"github.com/telemetryapp/gotelemetry_agent/agent/server"
+	"io/ioutil"
 	"log"
+	"os"
 
 	_ "github.com/telemetryapp/agent_crittercism/crittercism"
 	_ "github.com/telemetryapp/gotelemetry_agent/plugin"
 )
 
+var configFile *config.ConfigFile
+var errorChannel chan error
+var completionChannel chan bool
+
 func main() {
-	config, err := config.NewConfigFile()
-
-	if err != nil {
-		panic(err)
+	if config.CLIConfig.WantsFunctionHelp {
+		functions.PrintHelp(config.CLIConfig.FunctionHelpName)
+		return
 	}
 
-	errorChannel := make(chan error, 0)
-	completionChannel := make(chan bool, 0)
+	var err error
 
-	_, err = job.NewJobManager(config, errorChannel, completionChannel)
+	configFile, err = config.NewConfigFile()
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("Initialization error: %s", err)
 	}
+
+	errorChannel = make(chan error, 0)
+	completionChannel = make(chan bool, 0)
+
+	go run()
 
 	for {
 		select {
+		case err := <-errorChannel:
+			if e, ok := err.(*gotelemetry.Error); ok {
+				logLevel := e.GetLogLevel()
+
+				if logLevel >= config.CLIConfig.LogLevel {
+					prefix := "Error"
+
+					switch logLevel {
+					case gotelemetry.LogLevelLog:
+						prefix = "Info "
+
+					case gotelemetry.LogLevelDebug:
+						prefix = "Debug"
+					}
+
+					log.Printf("%s: %s", prefix, err)
+				}
+
+				continue
+			}
+
+			log.Printf("Error: %s", err.Error())
+
 		case <-completionChannel:
 			goto Done
-
-		case err := <-errorChannel:
-			log.Printf("Error: %s", err.Error())
 		}
 	}
 
 Done:
 
-	log.Println("No more jobs to run; exiting.")
+	log.Println("No more jobs to run; exiting.\n")
+}
+
+func run() {
+	if err := aggregations.Init(configFile.DataConfig().DataLocation, configFile.DataConfig().TTL, errorChannel); err != nil {
+		log.Fatalf("Initialization error: %s", err)
+	}
+
+	if err := graphite.Init(configFile, errorChannel); err != nil {
+		log.Fatalf("Initialization error: %s", err)
+	}
+
+	if config.CLIConfig.IsPiping {
+		payload, err := ioutil.ReadAll(os.Stdin)
+
+		if err != nil {
+			errorChannel <- err
+		}
+
+		agent.ProcessPipeRequest(configFile, errorChannel, completionChannel, payload)
+	} else if config.CLIConfig.IsNotifying {
+		agent.ProcessNotificationRequest(configFile, errorChannel, completionChannel, config.CLIConfig.NotificationChannel, config.CLIConfig.NotificationFlow, config.CLIConfig.Notification)
+	} else {
+		if configFile.ListenAddress() != "" {
+			if err := server.Init(configFile, errorChannel); err != nil {
+				log.Fatal("Web server initialization error: %s", err)
+			}
+		}
+
+		_, err := job.NewJobManager(configFile, errorChannel, completionChannel)
+
+		if err != nil {
+			log.Fatalf("Initialization error: %s", err)
+		}
+	}
 }
